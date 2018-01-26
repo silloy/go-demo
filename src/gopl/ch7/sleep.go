49e2f75edc6f211c5f3d5c4f6f06f53c90f7c280
SuShaohua <sshzh90@gmail.com>
2018-01-26 15:44:16 +0800

    interface

---
+++ src/gopl/ch7/sleep.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"flag"
+	"time"
+	"fmt"
+)
+
+var period = flag.Duration("period", 1* time.Second, "sleep period")
+
+func main()  {
+	flag.Parse()
+	fmt.Printf("Sleeping for %v...", *period)
+	time.Sleep(*period)
+	time.Sleep(time.Duration(5))
+	fmt.Println()
+}
