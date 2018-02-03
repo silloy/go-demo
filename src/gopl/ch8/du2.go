4701b6bbed42c56ea9c6aea07da88f8c626504e5
SuShaohua <sshzh90@gmail.com>
2018-02-03 16:18:43 +0800

    sync

---
+++ src/gopl/ch8/du2.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"flag"
+	"time"
+)
+
+var verbose = flag.Bool("v", false, "show verbose progress messages")
+
+func main() {
+	fileSizes := make(chan int64)
+
+	var tick <-chan time.Time
+	if *verbose {
+		tick = time.Tick(500 * time.Millisecond)
+	}
+	var nfiles, nbytes int64
+loop:
+	for {
+		select {
+		case size, ok := <- fileSizes:
+			if !ok {
+				break loop // fileSizes was closed
+			}
+			nfiles++
+			nbytes += size
+		case <-tick:
+			printDiskUsage(nfiles, nbytes)
+		}
+	}
+	printDiskUsage(nfiles, nbytes)
+}
+
+
